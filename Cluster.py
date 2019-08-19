6234432a41ebe25c5f5693b1f9d244a841d3b44f
KatyBrown <kab84@cam.ac.uk>
2019-08-19 20:08:24 +0100

    fixed clustering for single sequences

--- Cluster.py
+++ Cluster.py
@@ -29,10 +29,20 @@ def SWalign(seq1, seq2):
         return None
 
 
-def SWaligntest(seq1, seq2, nam1, nam2, min_score, min_perc, rscore=False):
+def SWaligntest(seq1, seq2, nam1, nam2, min_score, min_perc,
+                rscore=False):
+    '''
+    Test if two sequences are more similar than min_score and min_perc
+    using the Smith Waterman algorithm
+    
+    If one sequence overlaps the end of the other, min_perc doesn't
+    need to be satisfied but if one sequence is internal to the other
+    it does.
+    '''
     orig_len_1 = len(seq1)
     orig_len_2 = len(seq2)
 
+    # put the longer sequeunce first
     if orig_len_1 > orig_len_2:
         seq1, seq2 = seq2, seq1
         nam1, nam2 = nam2, nam1
@@ -85,28 +95,37 @@ def clusterCyclesSW(infile, outfile_prefix, outpath, final_out, batchsize=200,
                     min_score=6, min_perc=0.99,
                     min_length=50, keep_symbol="", calign_path="."):
 
+    # open an empty table to track how many sequences are in each cluster
     count_tab = open("%s/%s_clusters.tsv" % (outpath, outfile_prefix), "w")
+    # open the file which will be clustered
     F = ut_functions.FastaToDict(infile)
     currentF = dict()
+    # keep only the consensus sequences / only the sequences with the "keep"
+    # symbol
     for nam in F:
         if keep_symbol in nam or "cons" in nam:
             currentF[nam] = F[nam]
 
+    # make an output file with only the sequences which will be kept
     orig_out = "%s/%s_1.fasta" % (outpath, outfile_prefix)
     out = open(orig_out, "w")
     for key in currentF:
         out.write(">%s\n%s\n" % (key, currentF[key]))
     out.close()
 
+    # keep track of the length of the previous and the current list
+    # of sequences
     pfasta_len = len(currentF) + 1
     cfasta_len = len(currentF)
     
     p = 1
+    # while the number of sequences is decreasing
     while pfasta_len > cfasta_len:
         # split the fasta file into batches
         # the batches need to be quite big or nothing will cluster
         nbatches = len(currentF) / batchsize
         F_L = ut_functions.batchFasta(currentF, nbatches)
+        print (len(currentF), F_L)
         n = 1
         Cdict = dict()
         # for each batch of sequences
@@ -124,59 +143,85 @@ def clusterCyclesSW(infile, outfile_prefix, outpath, final_out, batchsize=200,
             seqs = [o[1] for o in z]
             G = nx.Graph()
             # make every pairwise comparison
+            # build a network with connections between sequences which are
+            # sufficiently similar
             for i, seq1 in enumerate(seqs):
+                nam1 = nams[i]
+                G.add_node(nam1)
                 for j, seq2 in enumerate(seqs[i+1:]):
-                    nam1 = nams[i]
                     nam2 = nams[j+i+1]
-                    G.add_node(nam1)
                     G.add_node(nam2)
+                    # test identity using the Smith-Waterman algorithm
                     ali_test = SWaligntest(seq1, seq2,
                                            nam1, nam2,
                                            min_score, min_perc)
                     if ali_test:
                         G.add_edge(nam1, nam2)
+    
+            # extract the connected groups from the network
             ccs = nx.connected_components(G)
             cdict = dict()
+            # for each cluster
             for cc in ccs:
+                print (cc)
+                # put all the sequences from the cluster into an output file
                 out = open("%s/%s_%s_%i.fasta" % (outpath, outfile_prefix, n+1, p), "w")
                 m = 0
                 for val in cc:
                     out.write(">%s\n%s\n" % (val, currentF[val]))
                     m += 1
-                out.close()        
+                out.close()
+                # if there is only one sequence in the cluster
                 if m == 1:
                     x = ut_functions.FastaToDict("%s/%s_%s_%i.fasta" % (outpath,
                                                                         outfile_prefix,
                                                                      n+1, p))
                     if len(x) != 0:
+                        # copy the sequence into the output file
                         out2 = open("%s/%s_%s_%i_consensus.fasta" % (outpath, outfile_prefix,
                                                                   n+1, p), "w")
                         out2.write(">%s_%s_%i_cons\n%s\n" % (outfile_prefix, n+1, p, list(x.values())[0]))
                         out2.close()
+                        # keep track of how many sequences this represents
                         count_tab.write("%s\t%s_%s_%i\n" % (list(x.keys())[0], outfile_prefix, n+1, p))
                         cdict.update(ut_functions.FastaToDict("%s/%s_%s_%i_consensus.fasta" % (outpath, outfile_prefix, n+1, p)))
                         n += 1
                 elif m > 1:
+                        # if there is more than one sequence in the cluster, check
+                        # if it should be subdivided by reclustering using a different
+                        # algorithm and see if it forms one cluster or more than one
+
                         # align the current set of sequences
                         statement = '''mafft --localpair %s/%s_%s_%i.fasta \
                         > %s/%s_%s_%i_ali_test.fasta''' % (outpath, outfile_prefix, n+1, p,
                         outpath, outfile_prefix, n+1, p)
                         os.system(statement)
                         arr, nams = CIAlign.FastaToArray("%s/%s_%s_%i_ali_test.fasta" % (outpath, outfile_prefix, n+1, p))
-                        ident = CIAlign.calculateSimilarityMatrix(arr, nams, outfile="/run/media/katy/HD_4/temp/mat.txt",
+                        
+                        # calculate the pairwise similarity between the current set of sequecnes
+                        ident = CIAlign.calculateSimilarityMatrix(arr, nams, outfile="%s/%s_%s_%i_mat.txt" % (outpath, outfile_prefix, n+1, p),
                                                                   minoverlap=10, keepgaps=False)
                         ident = np.nan_to_num(ident)
+                        
+                        # convert the distance between the sequences into a set of multi-dimensional scaling co-ordinates
                         M = sklearn.manifold.MDS(5, dissimilarity='precomputed')
                         m = M.fit_transform(1-ident)
+                        
+                        # apply agglomerative clustering to the sequences
                         c = sklearn.cluster.AgglomerativeClustering(n_clusters=None, distance_threshold=0.25).fit_predict(m)
+                        
+                        # for each subcluster
                         for i in range(max(c) + 1):
                             out = open("%s/%s_%s_%i.fasta" % (outpath, outfile_prefix, n+1, p), "w")
                             m = 0
                             t = np.array(nams)[c == i]
+                            # put the current subcluster into a file
                             for val in t:
                                 out.write(">%s\n%s\n" % (val, currentF[val]))
                                 m += 1
                             out.close()
+                            # if there is one sequence in the subcluster, just copy
+                            # it to make the consensus
                             if m == 1:
                                 x = ut_functions.FastaToDict("%s/%s_%s_%i.fasta" % (outpath,
                                                                                     outfile_prefix,
@@ -190,6 +235,8 @@ def clusterCyclesSW(infile, outfile_prefix, outpath, final_out, batchsize=200,
                                     cdict.update(ut_functions.FastaToDict("%s/%s_%s_%i_consensus.fasta" % (outpath, outfile_prefix, n+1, p)))
                                     n += 1
                             elif m > 1:
+                                # if there is more than one sequence in the subcluster,
+                                # align it and generate a consensus
                                 statement = '''mafft --localpair %s/%s_%s_%i.fasta \
                                 > %s/%s_%s_%i_ali.fasta''' % (outpath, outfile_prefix, n+1, p,
                                 outpath, outfile_prefix, n+1, p)
@@ -203,13 +250,17 @@ def clusterCyclesSW(infile, outfile_prefix, outpath, final_out, batchsize=200,
                                 p, min_length, outpath, outfile_prefix, n+1, p, outfile_prefix, n+1, p)
                                 ut_functions.writeCommand(statement, "%s/%s" % (outpath, outfile_prefix))
                                 os.system(statement)
+                                ###### !!!!!!
+                                # sometimes there is no sequence left after the previous step
                                 if os.path.exists("%s/%s_%s_%i_consensus.fasta"  %(outpath, outfile_prefix, n+1, p)):
                                     x = ut_functions.FastaToDict("%s/%s_%s_%i_parsed.fasta" % (outpath,
                                                                                                outfile_prefix,
                                                                                                n+1, p))
                                     for key in x:
+                                        # keep count of the number of sequences represented
                                         count_tab.write("%s\t%s_%s_%i\n" % (key, outfile_prefix, n+1, p))
                                     if os.path.exists("%s/%s_%s_%i_removed.txt" % (outpath, outfile_prefix, n+1, p)):
+                                        # keep track of which sequences have been removed because they didn't match well
                                         lines = [line.strip() for line in open("%s/%s_%s_%i_removed.txt" % (outpath, outfile_prefix, n+1, p)).readlines()]
                                         for line in lines:
                                             count_tab.write("%s\tremoved\n" % (line))
@@ -218,6 +269,7 @@ def clusterCyclesSW(infile, outfile_prefix, outpath, final_out, batchsize=200,
                 
             Cdict.update(cdict)
         p += 1
+        # Cdict contains all the consensus sequences
         out = open("%s/%s_%i.fasta" % (outpath, outfile_prefix, p), "w")
         for val in Cdict:
             out.write(">%s\n%s\n" % (val, Cdict[val]))
@@ -325,7 +377,7 @@ def expandClusters(infile_fasta, infile_clusters,
     bigF = dict()
     for line in clusters:
         line = line.strip().split("\t")
-        if "~" in line[0]:
+        if "_samples_" in line[0]:
             D[line[0]] = line[1]
             D2.setdefault("%s_cons" % line[1], [])
             D2['%s_cons' % line[1]].append(line[0])
