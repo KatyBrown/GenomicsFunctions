34707c8373fd20b6775312fdded92085ac1ffe1a
KatyBrown <thekatybrown@gmail.com>
2022-08-08 11:28:49 +0100

    added spades to Assembly.py

--- Assembly.py
+++ Assembly.py
@@ -8,6 +8,7 @@ import ruffus.cmdline as cmdline
 import pathlib
 import ut_functions
 import Run
+import os
 
 
 def runTrinity(infiles, pref, ispaired, strand, outfile, threads=1,
@@ -54,4 +55,63 @@ def runTrinity(infiles, pref, ispaired, strand, outfile, threads=1,
     ut_functions.writeCommand(statement, pref)
     Run.systemRun(statement, syst)
     ut_functions.writeTime("Trinity", "end", pref)
+    pathlib.Path(outfile).touch()
+
+
+def runSpades(infiles, pref, ispaired, outfile, threads=1,
+              mem=4, syst=""):
+    '''
+    Generates and runs a statement to run the rnaspades assembly software
+    
+    
+    Parameters
+    ----------
+    infiles: list
+        list of three input files [mate1, mate2, singleend]
+
+    pref: str
+        filename prefix (used for the log files)
+    ispaired: bool
+        True if paired end else False
+    outfile: str
+        path to output file, this is a placeholder to show the task has run
+    threads: int
+        number of threads with which to run spades
+    mem: str
+        maximum memory to pass to spades (formated as an integer)
+    syst: str
+        system option to run statements on different systems    
+    '''
+    in1, in2, in3 = infiles
+    ut_functions.writeTime("Spades", "start", pref)
+
+    outstem1 = "%s_rnaspades" % ".".join(outfile.split(".")[:-1])
+    outstem2 = "%s_rnaviralspades" % ".".join(outfile.split(".")[:-1])
+    log1 = "%s.log" % outstem1
+    log2 = "%s.log" % outstem2
+    if ispaired:
+        f1 = in1.replace(".fq.1", ".1.fq")
+        f2 = in2.replace(".fq.2", ".2.fq")
+        if not os.path.exists(f1):
+            os.symlink(in1.split("/")[-1], f1)
+        if not os.path.exists(f2):
+            os.symlink(in2.split("/")[-1], f2)
+        statement1 = '''rnaspades.py -1 %(f1)s -2 %(f2)s \
+                       -o %(outstem1)s -t %(threads)s -m %(mem)s \
+                       2>%(log1)s''' % locals()
+        statement2 = '''rnaviralspades.py -1 %(f1)s -2 %(f2)s \
+                       -o %(outstem2)s -t %(threads)s -m %(mem)s \
+                       2>%(log2)s''' % locals()
+    else:
+        statement1 = '''rnaspades.py -s %(in3)s\
+                       -o %(outstem1)s -t %(threads)s -m %(mem)s \
+                       2>%(log1)s''' % locals()
+        statement2 = '''rnaviralspades.py -s %(in1)s\
+                       -o %(outstem2)s -t %(threads)s -m %(mem)s \
+                       2>%(log2)s''' % locals()
+    ut_functions.writeCommand(statement1, pref)
+    ut_functions.writeCommand(statement2, pref)
+    Run.systemRun(statement1, syst)
+    Run.systemRun(statement2, syst)
+    ut_functions.writeTime("Spades", "end", pref)
     pathlib.Path(outfile).touch()
