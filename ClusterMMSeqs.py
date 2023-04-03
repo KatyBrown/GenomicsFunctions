798f784bf67488084618ad969f3775df80797612
Katy Brown <kab84@cam.ac.uk>
2023-04-03 13:29:26 +0100

    added table/fasta section to cluster MMseqs

--- ClusterMMSeqs.py
+++ ClusterMMSeqs.py
@@ -1,6 +1,7 @@
-import pathlib
 import subprocess
-import shutil
+import pandas as pd
+import os
+import ut_functions
 
 
 def makeDB(infile, dbout, statementout, sout, slog):
@@ -44,3 +45,34 @@ def makeTxt(clusters, db, outfile, statementout, sout, slog):
 
 
 
+def makeTablesFastas(lookup, txtfile, orig_fasta, rep_fasta_out, tab_out, fastadir):
+    try:
+        os.mkdir(fastadir)
+    except:
+        pass
+    F = ut_functions.FastaToDict(orig_fasta)
+    inds = pd.read_csv(lookup, sep="\t", header=None, index_col=0)
+    indD = dict(zip(inds.index.values, inds[1]))
+    tab = pd.read_csv(txtfile, sep="\t", header=None)
+    tab.columns = ['Rep_Name', 'Sequence_Ind']
+    tab['Cluster_Members'] = [indD[x] for x in tab['Sequence_Ind']]
+    clustered = tab.groupby('Rep_Name')
+    outr = open(rep_fasta_out, "w")
+    outt = open(tab_out, "w")
+
+    outt.write("group_id\tgroup_member\tgroup_rep\tgroup_size\n")
+    gn = 1
+    for group in set(tab.Rep_Name):
+        groupt = clustered.get_group(group)
+        group_id = "g%i" % (gn)
+        group_size = len(groupt)
+        out = open("%s/%s.fasta" % (fastadir, group.replace("|", "_")), "w")
+        for member in groupt['Cluster_Members']:
+            outt.write("%s\t%s\t%s\t%s\n" % (group_id, member, group,
+                                             group_size))
+            out.write(">%s_%s\n%s\n" % (group_id, member, F[member]))
+        out.close()
+        outr.write(">%s|%s\n%s\n" % (group_id, group, F[group]))
+        gn += 1
+    outr.close()
+    outt.close()
